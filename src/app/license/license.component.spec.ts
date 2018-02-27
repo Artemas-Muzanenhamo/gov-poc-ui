ea9115903c1ff006dd9d31397a4c8fefb8c9b0a9
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-02-27 16:00:50 +0000

    add LicenseComponent test cases

--- src/app/license/license.component.spec.ts
+++ src/app/license/license.component.spec.ts
@@ -9,4 +9,22 @@ describe('LicenseComponent', () => {
       ],
     }).compileComponents();
   }));
+  it('should render an Add License button', async( () => {
+    const fixture = TestBed.createComponent(LicenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#add-license-details').textContent).toContain('Add License');
+  }));
+  it('should render an Edit License button', async( () => {
+    const fixture = TestBed.createComponent(LicenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#edit-license-details').textContent).toContain('Edit License');
+  }));
+  it('should render a Delete License button', async( () => {
+    const fixture = TestBed.createComponent(LicenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button#delete-license-details').textContent).toContain('Delete License');
+  }));
 });
