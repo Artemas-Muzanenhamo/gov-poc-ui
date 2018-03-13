4fdd95ef67d71f87676fa701037af286c578ba79
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-13 09:42:03 +0000

    polish up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import {IdentityComponent} from './identity/identity.component';
 import {WelcomeComponent} from './welcome/welcome.component';
 import {AddLicenseComponent} from './license/add-license/add-license.component';
 import {LicensesComponent} from './license/licenses/licenses.component';
-import {EditLicenseComponent} from "./license/edit-license/edit-license.component";
+import {EditLicenseComponent} from './license/edit-license/edit-license.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent},
