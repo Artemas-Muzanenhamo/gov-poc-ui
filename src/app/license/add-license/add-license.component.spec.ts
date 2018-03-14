6ea2f65113e249fe81e17de5d8c1fdafe187b2af
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-14 08:35:55 +0000

    add License First Names Input test case

--- src/app/license/add-license/add-license.component.spec.ts
+++ src/app/license/add-license/add-license.component.spec.ts
@@ -51,7 +51,12 @@ describe('AddLicenseComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('#surname-input').nodeName).toBe('INPUT');
   }));
-  it('should render a FIRST NAME field in the Add License Form');
+  it('should render a FIRST NAME field in the Add License Form', async( () => {
+    const fixture = TestBed.createComponent(AddLicenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#first-name-input').nodeName).toBe('INPUT');
+  }));
   it('should render a DATE OF BIRTH field in the Add License Form');
   it('should render a COUNTRY field in the Add License Form');
   it('should render a DATE OF ISSUE field in the Add License Form');
