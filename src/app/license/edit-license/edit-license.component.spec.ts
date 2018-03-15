27c18e292804c39dc1cee07d6020d3f197c04cab
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-15 16:31:49 +0000

    add EditLicense input test case

--- src/app/license/edit-license/edit-license.component.spec.ts
+++ src/app/license/edit-license/edit-license.component.spec.ts
@@ -63,7 +63,12 @@ describe('EditLicenseComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('#date-of-birth').nodeName).toBe('INPUT');
   }));
-  it('should render a COUNTRY field in the Edit License Form');
+  it('should render a COUNTRY field in the Edit License Form', async( () => {
+    const fixture = TestBed.createComponent(EditLicenseComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#country').nodeName).toBe('INPUT');
+  }));
   it('should render a DATE OF ISSUE field in the Edit License Form');
   it('should render an EXPIRY DATE field in the Edit License Form');
   it('should render an AGENCY field in the Edit License Form');
