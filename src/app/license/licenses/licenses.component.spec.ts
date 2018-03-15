a66d09ac9f278102ce77a8a3f8374637da13da3d
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-15 17:04:33 +0000

    add LicensesComponent tests cases

--- src/app/license/licenses/licenses.component.spec.ts
+++ src/app/license/licenses/licenses.component.spec.ts
@@ -6,6 +6,7 @@ import {IdentityComponent} from '../../identity/identity.component';
 import {LicensesComponent} from './licenses.component';
 import {AppRoutingModule} from '../../app-routing.module';
 import {APP_BASE_HREF} from '@angular/common';
+import {EditLicenseComponent} from '../edit-license/edit-license.component';
 
 describe('LicensesComponent', () => {
   beforeEach(async(() => {
@@ -15,7 +16,8 @@ describe('LicensesComponent', () => {
         AddLicenseComponent,
         WelcomeComponent,
         IdentityComponent,
-        LicensesComponent
+        LicensesComponent,
+        EditLicenseComponent
       ],
       imports: [
         AppRoutingModule
@@ -25,16 +27,76 @@ describe('LicensesComponent', () => {
       ]
     }).compileComponents();
   }));
-  it('should render a table to display license details');
-  it('should render an ID field in the License Table Header');
-  it('should render an ID REFERENCE field in the License Table Header');
-  it('should render a SURNAME field in the License Table Header');
-  it('should render a FIRSTNAME field in the License Table Header');
-  it('should render a DATE OF BIRTH field in the License Table Header');
-  it('should render a COUNTRY field in the License Table Header');
-  it('should render a DATE OF ISSUE field in the License Table Header');
-  it('should render an EXPIRY DATE field in the License Table Header');
-  it('should render an AGENCY field in the License Table Header');
-  it('should render a LICENSE NUMBER field in the License Table Header');
-  it('should render an ADDRESS field in the License Table Header');
+  it('should render a table to display license details', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#licenses-table').nodeName).toBe('TABLE');
+  }));
+  it('should render an ID field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#license-id').nodeName).toBe('TH');
+  }));
+  it('should render an ID REFERENCE field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#id-reference').nodeName).toBe('TH');
+  }));
+  it('should render a SURNAME field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#surname').nodeName).toBe('TH');
+  }));
+  it('should render a FIRSTNAME field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#first-name').nodeName).toBe('TH');
+  }));
+  it('should render a DATE OF BIRTH field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#date-of-birth').nodeName).toBe('TH');
+  }));
+  it('should render a COUNTRY field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#country').nodeName).toBe('TH');
+  }));
+  it('should render a DATE OF ISSUE field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#date-of-issue').nodeName).toBe('TH');
+  }));
+  it('should render an EXPIRY DATE field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#expiry-date').nodeName).toBe('TH');
+  }));
+  it('should render an AGENCY field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#agency').nodeName).toBe('TH');
+  }));
+  it('should render a LICENSE NUMBER field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#license-number').nodeName).toBe('TH');
+  }));
+  it('should render an ADDRESS field in the License Table Header', async( () => {
+    const fixture = TestBed.createComponent(LicensesComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#address').nodeName).toBe('TH');
+  }));
 });
