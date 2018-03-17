898e10652656c150ae6c2365d1928a62c4a3a8ed
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-17 00:17:16 +0000

    add IdentitiesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {AddLicenseComponent} from './license/add-license/add-license.component';
 import {LicensesComponent} from './license/licenses/licenses.component';
 import {EditLicenseComponent} from './license/edit-license/edit-license.component';
+import {IdentitiesComponent} from './identity/identities/identities.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import {EditLicenseComponent} from './license/edit-license/edit-license.componen
     WelcomeComponent,
     AddLicenseComponent,
     LicensesComponent,
-    EditLicenseComponent
+    EditLicenseComponent,
+    IdentitiesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/identity/identities/identities.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-identities',
+  templateUrl: './identities.component.html'
+})
+export class IdentitiesComponent {}
