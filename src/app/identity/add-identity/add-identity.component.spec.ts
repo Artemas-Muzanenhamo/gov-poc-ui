caa0c7c80872dbfc22fcadf7ad3b701eea012374
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-03-21 10:34:22 +0000

    add AddIdentityComponent test cases

--- src/app/identity/add-identity/add-identity.component.spec.ts
+++ src/app/identity/add-identity/add-identity.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddIdentityComponent } from './add-identity.component';
 
-fdescribe('AddIdentityComponent', () => {
+describe('AddIdentityComponent', () => {
   let component: AddIdentityComponent;
   let fixture: ComponentFixture<AddIdentityComponent>;
   let compiled;
@@ -26,5 +26,29 @@ fdescribe('AddIdentityComponent', () => {
   });
   it('should render Add Identity form', async( () => {
     expect(compiled.querySelector('#identity-form').nodeName).toContain('FORM');
+  }));
+  it('should render ID Number as input', async( () => {
+    expect(compiled.querySelector('#identity-number').nodeName).toContain('INPUT');
+  }));
+  it('should render ID Reference as input', async( () => {
+    expect(compiled.querySelector('#identity-ref').nodeName).toContain('INPUT');
+  }));
+  it('should render First Names as input', async( () => {
+    expect(compiled.querySelector('#first-names').nodeName).toContain('INPUT');
+  }));
+  it('should render Surname as input', async( () => {
+    expect(compiled.querySelector('#surname').nodeName).toContain('INPUT');
+  }));
+  it('should render Date of Birth as input', async( () => {
+    expect(compiled.querySelector('#date-of-birth').nodeName).toContain('INPUT');
+  }));
+  it('should render Village of Origin as input', async( () => {
+    expect(compiled.querySelector('#village-of-origin').nodeName).toContain('INPUT');
+  }));
+  it('should render Place of Birth as input', async( () => {
+    expect(compiled.querySelector('#place-of-birth').nodeName).toContain('INPUT');
+  }));
+  it('should render Date of Issue as input', async( () => {
+    expect(compiled.querySelector('#date-of-issue').nodeName).toContain('INPUT');
   }));
 });
