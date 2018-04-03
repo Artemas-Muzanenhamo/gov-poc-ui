7344f10a563d5c388b1430a5abb32c2ffd7bce57
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-03 16:29:59 +0100

    add IdentityModule

--- src/app/identity/identity.module.ts
+++ src/app/identity/identity.module.ts
@@ -1,1 +1,21 @@
+import {NgModule} from '@angular/core';
+import {EditIdentityComponent} from './edit-identity/edit-identity.component';
+import {AddIdentityComponent} from './add-identity/add-identity.component';
+import {IdentitiesComponent} from './identities/identities.component';
+import {IdentityComponent} from './identity.component';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    IdentitiesComponent,
+    AddIdentityComponent,
+    EditIdentityComponent,
+    IdentityComponent,
+  ],
+  imports: [
+    RouterModule.forChild([
+      {path: 'identity', component: IdentityComponent}
+    ])
+  ]
+})
 export class IdentityModule { }
