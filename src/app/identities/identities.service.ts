b6cac64beeb1aa8166e764a5c7404f6c59789bbf
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-05-19 15:34:19 +0100

    polish up IdentityService component

--- src/app/identities/identities.service.ts
+++ src/app/identities/identities.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Identity} from './identity/identity';
+import * as moment from 'moment';
 
 @Injectable()
 export class IdentityService {
@@ -9,40 +10,40 @@ export class IdentityService {
       identityRef: '6678944',
       name: 'Artemas',
       surname: 'Muzanenhamo',
-      birthDate: '28/03/1990',
+      birthDate: moment.utc('28/03/1990', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0],
       villageOfOrigin: 'Mashayamombe',
       placeOfBirth: 'Zimbabwe',
-      dateOfIssue: '01/01/2018'
+      dateOfIssue: moment.utc('01/01/2018', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0]
     },
     {
       id: 'MUZAn09876',
       identityRef: '6678944',
       name: 'Artemas',
       surname: 'Muzanenhamo',
-      birthDate: '28/03/1990',
+      birthDate: moment.utc('28/03/1990', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0],
       villageOfOrigin: 'Mashayamombe',
       placeOfBirth: 'Zimbabwe',
-      dateOfIssue: '01/01/2018'
+      dateOfIssue: moment.utc('01/01/2018', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0]
     },
     {
       id: 'MUZAn09876',
       identityRef: '6678944',
       name: 'Artemas',
       surname: 'Muzanenhamo',
-      birthDate: '28/03/1990',
+      birthDate: moment.utc('28/03/1990', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0],
       villageOfOrigin: 'Mashayamombe',
       placeOfBirth: 'Zimbabwe',
-      dateOfIssue: '01/01/2018'
+      dateOfIssue: moment.utc('01/01/2018', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0]
     },
     {
       id: 'MUZAn09876',
       identityRef: '6678944',
       name: 'Artemas',
       surname: 'Muzanenhamo',
-      birthDate: '28/03/1990',
+      birthDate: moment.utc('28/03/1990', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0],
       villageOfOrigin: 'Mashayamombe',
       placeOfBirth: 'Zimbabwe',
-      dateOfIssue: '01/01/2018'
+      dateOfIssue: moment.utc('01/01/2018', 'DD/MM/YYYY', true).toDate().toISOString().split('T')[0]
     }
   ];
 
