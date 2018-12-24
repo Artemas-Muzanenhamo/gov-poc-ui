e4b60e758ce81b44ebe70a05d193330685d3f8a3
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-12-24 23:17:31 +0000

    move delete button to its own link

--- src/app/identities/delete-identity/delete-identity.component.ts
+++ src/app/identities/delete-identity/delete-identity.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import {Identity} from '../identity/identity';
 import {IdentityService} from '../identities.service';
 
