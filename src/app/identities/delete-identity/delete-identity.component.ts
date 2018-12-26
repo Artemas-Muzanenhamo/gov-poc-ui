18d0946861f0c11dfb3b421cec13b735638d6c3a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2018-12-25 22:45:57 -1100

    add angular-material

--- src/app/identities/delete-identity/delete-identity.component.ts
+++ src/app/identities/delete-identity/delete-identity.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Identity} from '../identity/identity';
 import {IdentityService} from '../identities.service';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 @Component({
   selector: 'app-delete-identity',
@@ -13,7 +14,8 @@ export class DeleteIdentityComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private identityService: IdentityService
+    private identityService: IdentityService,
+    public dialogRef: MatDialogRef<DeleteIdentityComponent>,
   ) { }
 
   ngOnInit(): void {
