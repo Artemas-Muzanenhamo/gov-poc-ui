4d0f6f5ffae56111e7f61b267f94ece395a02bc6
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-01-21 08:50:05 +0000

    amend Identity-Detail Component test

--- src/app/identities/identity-detail/identity-detail.component.ts
+++ src/app/identities/identity-detail/identity-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Identity} from '../identity/identity';
-import {MatDialog, MatDialogConfig} from '@angular/material';
 
 @Component({
   selector: 'app-identity-detail',
@@ -16,8 +15,7 @@ export class IdentityDetailComponent implements OnInit {
   deleteButton: String = 'Delete';
 
   constructor(
-    private route: ActivatedRoute,
-    private dialog: MatDialog
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
