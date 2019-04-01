c1dece6aa976e0c6ee590dfc6cc124019196c74e
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-04-01 19:33:10 +0100

    Add more test setup for Delete Identity Component

--- src/app/identities/delete-identity-dialog/delete-identity.component.spec.ts
+++ src/app/identities/delete-identity-dialog/delete-identity.component.spec.ts
@@ -3,8 +3,14 @@ import {AppModule} from '../../app.module';
 import {RouterTestingModule} from '@angular/router/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {IdentityService} from '../identities.service';
+import {DeleteIdentityDialogComponent} from './delete-identity-dialog.component';
 
 describe('DeleteIdentityComponent', () => {
+  let fixture;
+  let identityService;
+  let component;
+  let compiled;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -16,5 +22,15 @@ describe('DeleteIdentityComponent', () => {
         IdentityService
       ]
     }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeleteIdentityDialogComponent);
+    identityService = TestBed.get(IdentityService);
+    component = fixture.componentInstance;
+    compiled = fixture.debugElement.nativeElement;
+  });
+
+  it('should display modal with YES button', async(() => {
   }));
 });
